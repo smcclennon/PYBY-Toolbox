24edf8b677e0ed2d1a4925cf039dbc11a81b98ef
Shiraz <24913281+smcclennon@users.noreply.github.com>
2020-03-02 20:53:53 +0000

    Supports multithreading

--- Volute.py
+++ Volute.py
@@ -1,23 +1,42 @@
 #Volute
-#github.com/smcclennon
-ver='1'
+#github.com/smcclennon/PYBY-Toolbox
+ver='1.2.0'
+
+THREADS_TO_CREATE = 4
 
 #import
 try:
     print('Importing required libraries...')
-    import os,urllib.request,json
-    from ctypes import cast, POINTER; from comtypes import CLSCTX_ALL 
+    import os
+    from threading import Thread
+    from ctypes import cast, POINTER
     os.system('title Volute v'+ver+' - Importing...')
 except:
     print('\nError: unable to import one or more libraries\nVisit: github.com/smcclennon/PYBY-Toolbox for support\n\nPress enter to exit...')
     input()
     exit()
+
+try:
+    from comtypes import CLSCTX_ALL
+except:
+    print('\nError: unable to import "comtypes"')
+    confirm=input(str('Attempt to install "comtypes"? [Y/n] ')).upper()
+    if confirm!='N':
+        try:
+            os.system('pip install comtypes --user')
+            os.system('cls')
+            os.system('"'+str(os.path.basename(__file__))+'"')
+            exit()
+        except:
+            print('Failed to install "pycaw"\nPress enter to exit...')
+        input()
+        exit()
 try:
     from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 except:
     print('\nError: unable to import "pycaw"')
     confirm=input(str('Attempt to install "pycaw"? [Y/n] ')).upper()
-    if confirm=='Y':
+    if confirm!='N':
         try:
             os.system('pip install pycaw --user')
             os.system('cls')
@@ -35,9 +54,22 @@ devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(
    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
-os.system('title Volute v'+ver)
-os.system('cls')
-print('Unmuting your system!\nTo stop, close this script.')
-while True:
-   volume.SetMute(0, None)
 
+run = True
+
+def unmuteThread():
+    while run == True:
+        volume.SetMute(0, None)
+
+os.system('title Volute v'+ver)
+print(f'Creating {THREADS_TO_CREATE} threads...')
+
+for i in range(0, THREADS_TO_CREATE):
+    Thread(target = unmuteThread).start()
+    print('Created thread')
+
+os.system('cls')
+print(f'Unmuting your system with {THREADS_TO_CREATE} threads!\n\nPress ENTER to stop')
+input()
+print(f'Stopping {THREADS_TO_CREATE} threads...')
+run = False
